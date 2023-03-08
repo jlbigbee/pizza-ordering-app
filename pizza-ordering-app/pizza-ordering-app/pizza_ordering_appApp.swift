02724bac1f8b6c18356fd0fac3659a3330f8a671
Jonathan Bigbee <bigbee.jonathan@gmail.com>
2023-03-08 12:29:36 -0800

    created playground

---
+++ pizza-ordering-app/pizza-ordering-app/pizza_ordering_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  pizza_ordering_appApp.swift
+//  pizza-ordering-app
+//
+//  Created by Jonathan Bigbee on 3/8/23.
+//
+
+import SwiftUI
+
+@main
+struct pizza_ordering_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
